c889fa28e2ea3f7b378ccd6a47296f96df4a431d
RichardThomas215 <richardthomas215@yahoo.com>
2018-01-01 17:18:30 -0500

    Add text label

--- Flash Cards/src/FlashCard/AddingWindow.java
+++ Flash Cards/src/FlashCard/AddingWindow.java
@@ -1,14 +1,29 @@
 package FlashCard;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class AddingWindow extends JFrame {
 	
 	public AddingWindow() {
 		
 		super("Please add your card");
-		setSize(400, 200);
 		
+		JPanel p1 = new JPanel();
+		
+		JLabel front = new JLabel("Front");
+		p1.add(front);
+		add(p1);
+		
+		setSize(400, 200);
+		setVisible(true);
 	}
 
+	public static void main(String []args) {
+		
+		AddingWindow w = new AddingWindow();
+		
+	}
+ 
 }
