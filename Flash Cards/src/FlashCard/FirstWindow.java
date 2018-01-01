56acc1f57c78d2fe52a32ae94ca37f9791a51fb2
RichardThomas215 <richardthomas215@yahoo.com>
2018-01-01 11:50:45 -0500

    continued working on first panel

--- Flash Cards/src/FlashCard/FirstWindow.java
+++ Flash Cards/src/FlashCard/FirstWindow.java
@@ -1,11 +1,28 @@
 package FlashCard;
 
+import java.awt.Panel;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class FirstWindow extends JFrame {
 	
 	public FirstWindow() {
 		
+		JFrame first = new JFrame("Welcome");
+		Panel p = new Panel();
+		JButton addB = new JButton("add");
+		p.add(addB);
+		
+		first.add(p);
+		 
+		first.setSize(400, 400);
+		first.setVisible(true);
+	}
+	
+	public static void main(String [] args) {
+		
+		FirstWindow f = new FirstWindow();
 	}
 
 }
