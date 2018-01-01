53fa30c11233de17b35ad860225ffa10effa7ed6
RichardThomas215 <richardthomas215@yahoo.com>
2018-01-01 15:35:38 -0500

    Cleaned up and added gbc

--- Flash Cards/src/FlashCard/FirstWindow.java
+++ Flash Cards/src/FlashCard/FirstWindow.java
@@ -1,23 +1,36 @@
 package FlashCard;
 
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 import java.awt.Panel;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class FirstWindow extends JFrame {
 	
 	public FirstWindow() {
 		
-		JFrame first = new JFrame("Welcome");
-		Panel p = new Panel();
+		super("Welcome");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(15,15, 15, 0);
+		
+		
+		JPanel p = new JPanel();
+		
+		
 		JButton addB = new JButton("add");
+		
 		p.add(addB);
 		
-		first.add(p);
+		add(p,BorderLayout.CENTER);
 		 
-		first.setSize(400, 400);
-		first.setVisible(true);
+		setSize(400, 400);
+		setVisible(true);
 	}
 	
 	public static void main(String [] args) {
