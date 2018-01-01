f0de87cc2654fd5b4e3241ff33480df4fecfdeee
RichardThomas215 <richardthomas215@yahoo.com>
2018-01-01 17:31:57 -0500

    Added text label for userinput behind a card

--- Flash Cards/src/FlashCard/AddingWindow.java
+++ Flash Cards/src/FlashCard/AddingWindow.java
@@ -1,8 +1,11 @@
 package FlashCard;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class AddingWindow extends JFrame {
 	
@@ -12,9 +15,26 @@ public class AddingWindow extends JFrame {
 		
 		JPanel p1 = new JPanel();
 		
+		// the word Front
 		JLabel front = new JLabel("Front");
 		p1.add(front);
+		
+		// text field to enter the front
+		JTextField jt = new JTextField(10);
+		p1.add(jt, BorderLayout.AFTER_LAST_LINE);
 		add(p1);
+		
+		JPanel p2 = new JPanel();
+		
+		// the word back
+		JLabel back = new JLabel("Back");
+		p2.add(back);
+		
+		//text field to enter the back
+		JTextField jt2 = new JTextField(10);
+		p2.add(jt2, BorderLayout.AFTER_LAST_LINE);
+		add(p2, BorderLayout.SOUTH);
+		
 		
 		setSize(400, 200);
 		setVisible(true);
