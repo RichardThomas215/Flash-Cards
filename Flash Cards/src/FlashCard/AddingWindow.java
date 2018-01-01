711849421291d195ff319a6d55fb881737351d16
RichardThomas215 <richardthomas215@yahoo.com>
2018-01-01 15:35:13 -0500

    began workin on window to add cards

---
+++ Flash Cards/src/FlashCard/AddingWindow.java
@@ -0,0 +1,14 @@
+package FlashCard;
+
+import javax.swing.JFrame;
+
+public class AddingWindow extends JFrame {
+	
+	public AddingWindow() {
+		
+		super("Please add your card");
+		setSize(400, 200);
+		
+	}
+
+}
