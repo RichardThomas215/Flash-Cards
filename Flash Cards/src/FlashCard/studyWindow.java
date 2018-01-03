52be615631a13a7898d8430747db589fb1bbae10
RichardThomas215 <richardthomas215@yahoo.com>
2018-01-03 16:03:18 -0500

    began working on study windows

---
+++ Flash Cards/src/FlashCard/studyWindow.java
@@ -0,0 +1,14 @@
+package FlashCard;
+
+import javax.swing.JFrame;
+
+public class studyWindow extends JFrame {
+
+    public studyWindow() {
+    	
+    	super("Reviewing");
+    	setSize(200, 200);
+    	
+    	
+    }
+}
