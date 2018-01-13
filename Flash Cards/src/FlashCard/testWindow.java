04e70a2189e15b607645563a7c88dc7dd9d20b61
RichardThomas215 <richardthomas215@yahoo.com>
2018-01-13 14:56:08 -0500

    began working on test window

---
+++ Flash Cards/src/FlashCard/testWindow.java
@@ -0,0 +1,13 @@
+package FlashCard;
+
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+public class testWindow extends JFrame{
+	
+	public testWindow(ArrayList<Card> list) {
+		
+	}
+
+}
