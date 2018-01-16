afcc1794c44ba8e190500fbc59cfa7908435635e
RichardThomas215 <richardthomas215@yahoo.com>
2018-01-15 21:41:42 -0500

    Add and began working on swtiching from arraylist implementation to database implementation

---
+++ Flash Cards/src/FlashCard/DBConnect.java
@@ -0,0 +1,42 @@
+package FlashCard;
+
+import java.sql.*;
+
+public class DBConnect {
+
+	private Connection con;
+	private Statement st;
+	private ResultSet rs;
+	
+	public DBConnect() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb","root", "");
+			st = con.createStatement();
+			
+		}catch(Exception ex) {
+			System.out.println("Error: "+ex);
+		}
+	}
+	
+	public void getData() {
+		try {
+			
+			String query = "select * from cards";
+			rs = st.executeQuery(query);
+			System.out.println("Records from DataBase");
+			
+			while(rs.next()) {
+				String front = rs.getString("Front");
+				String back = rs.getString("Back");
+				
+				System.out.println("Front: "+ front);
+				System.out.println("Back: "+ back);
+			}
+			
+		}catch(Exception ex){
+			System.out.println(ex);
+		}
+	}
+}
